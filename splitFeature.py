a999b34fc6b70b011b14bb1397928618ba03ba38
Drew Bennett <Drew.Bennett@PTS.local>
2021-09-13 11:51:58 +0100

    done renaming

---
+++ splitFeature.py
@@ -0,0 +1,44 @@
+from PyQt5.QtCore import QVariant
+
+import numpy
+
+from qgis.core import QgsField,QgsFeature,QgsGeometry
+from . substring import substring
+
+
+def splitFeature(feature,step=10,field=None):
+    
+    fields=feature.fields()
+    fields.append(QgsField('start_chainage', QVariant.Double))
+    fields.append(QgsField('end_chainage', QVariant.Double))
+    
+    geomLength = feature.geometry().length()
+    
+    if field:
+        length = feature[field]
+    else:
+        length = geomLength
+    
+    #print('length:',length,'step:',step)
+    
+    chainages = numpy.arange(0,length,step)
+    
+    if chainages[-1] < length:
+        chainages = numpy.append(chainages,length)
+            
+    for i,v in enumerate(chainages[0:-1]):
+        f = QgsFeature(fields)
+        f.setAttributes(feature.attributes()+[float(v),float(chainages[i+1])])
+        
+        f.setGeometry(substring(feature.geometry()
+            ,v*geomLength/length
+            ,chainages[i+1]*geomLength/length))
+        yield f
+        
+
+if __name__ == '__console__':
+
+    layer = iface.activeLayer()   
+    feature = layer.selectedFeatures()[0]
+    print([f['start_chainage'] for f in splitFeature(feature)])
+    
---
+++ substring.py
@@ -0,0 +1,61 @@
+
+from qgis.core import QgsPoint,QgsGeometry
+
+#how the hell does qgis not have this?!
+#is in expression generator but how to use?
+
+#shapely version too old for shapely.substring.
+
+#QgsGeometry,start distance, end distance
+def substring(geometry,start,end):
+    
+    #print('start:',start,'end:',end)
+    geometry.convertToSingleType()
+        
+    length = geometry.length()
+   
+    if start>length:
+        raise ValueError('start > geometry length')
+
+    if end<0:
+        raise ValueError('end < 0')
+        
+    ch = 0
+    last = None
+    points = []
+    
+    s = QgsPoint()
+    s.fromWkt(geometry.interpolate(start).asWkt())
+    points.append(s)
+  
+    for v in geometry.vertices():
+        if not last is None:
+            ch += v.distance(last)
+        last = v
+        
+        if start<ch and ch<end:
+            points.append(v)
+            #print(v,ch)
+
+        if ch>=end:
+            break
+        
+    if end>length:
+        end = length
+        
+    e = QgsPoint()
+    e.fromWkt(geometry.interpolate(end).asWkt())
+    points.append(e)
+        
+    #print(points)
+    return QgsGeometry.fromPolyline(points)#qgspoints
+    #QgsLineString(points)
+
+if __name__ == '__console__':
+    layer = iface.activeLayer()
+        #fields = layer.fields()
+        #fields.append(QgsField('start_chainage', QVariant.Double))
+        #fields.append(QgsField('end_chainage', QVariant.Double))
+        
+    feature = layer.selectedFeatures()[0]
+    print(substring(feature.geometry(),0,10))
